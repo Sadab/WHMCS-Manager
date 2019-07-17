288e0c563e6a4c1f8f79e8e6dc903640460d9a8f
Sadab <sadabrahman@gmail.com>
2019-07-17 06:08:52 +0600

    FIXED MainActivity, ADDED GridLayout

--- app/src/main/java/com/codeian/whmcsmanager/MainActivity.java
+++ app/src/main/java/com/codeian/whmcsmanager/MainActivity.java
@@ -1,11 +1,16 @@
 package com.codeian.whmcsmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 import com.codeian.whmcsmanager.Adapters.ClientsAdapter;
 import com.codeian.whmcsmanager.Model.Client.Clients;
@@ -19,7 +24,8 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
+    /*private RecyclerView recyclerView;*/
+    GridLayout gridLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +33,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Bindings
-        recyclerView = findViewById(R.id.clientListView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        gridLayout = findViewById(R.id.gridLayout);
+/*        recyclerView = findViewById(R.id.clientListView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));*/
 
-        //Get Client List
-        getClientList();
+        /*//Get Client List
+        getClientList();*/
+
+        //Grid
+        setSingleEvent(gridLayout);
 
     }
 
-    private void getClientList(){
+    /*private void getClientList(){
 
         GetService service = ApiHandler.getRetrofitInstance().create(GetService.class);
         Call<DataModel> dataModelClient = service.getClients();
@@ -53,5 +63,24 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Error", t.getMessage());
             }
         });
+    }*/
+
+    private void setSingleEvent(GridLayout gridLayout){
+        for(int i = 0; i<gridLayout.getChildCount(); i++){
+            CardView cardView = (CardView) gridLayout.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(finalI == 0){
+                        Intent intent = new Intent(MainActivity.this, ClientList.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this,"Clicked @ position " + finalI, Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
     }
 }
